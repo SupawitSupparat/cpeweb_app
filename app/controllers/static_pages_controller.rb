b5334eafc216cd7a366e60bf3cced8bb4b40474c
Rommadon Teedo <rommadon.t@ku.th>
2017-05-25 07:22:20 +0000

    delete some page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,18 +2,5 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def help
-  end
-
-  def about
-  end
-  
-  def contact
-  end
-  
-  def login
-  end
-  
-  def singup
-  end
+ 
 end
